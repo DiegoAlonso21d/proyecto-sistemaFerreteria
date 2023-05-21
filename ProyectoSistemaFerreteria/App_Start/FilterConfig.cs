a0f8be9c266c528d3709f300e5a6d3093c3eb1a0
Diego Alonso <diegoalonso_21d@hotmail.com>
2023-05-20 21:38:59 -0500

    Primer commit

---
+++ ProyectoSistemaFerreteria/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoSistemaFerreteria
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ProyectoSistemaFerreteria/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoSistemaFerreteria.MvcApplication" Language="C#" %>
